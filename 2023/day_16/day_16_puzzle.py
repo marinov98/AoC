26f2f2cf255a2159ae51384b67d51758816e4838
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-06 13:33:23 -0500

    experimenting with part 2

--- 2023/day_16/day_16_puzzle.py
+++ 2023/day_16/day_16_puzzle.py
@@ -1,58 +1,72 @@
 def find_energized_tiles(grid: list) -> int:
-    energized_tiles = 0
-    beams = [(str("r"), int(0), int(0))]
-    visited = set()
-    tile_tracker = set()
     rows = len(grid)
     cols = len(grid[0])
-    while len(beams) != 0:
-        d, r, c = beams.pop()
-        while -1 < r < rows and -1 < c < cols and (d, r, c) not in visited:
-            # print(f"row: {r} col: {c} elem: {grid[r][c]} beams: {beams}")
-            visited.add((d, r, c))
-            if (r, c) not in tile_tracker:
-                energized_tiles += 1
-                tile_tracker.add((r, c))
-            if grid[r][c] in "/\\|-":
-                # logic for adding beams and changing directions
-                tile = grid[r][c]
-                if tile == "/":
-                    if d == "r":
-                        d = "u"
-                    elif d == "l":
-                        d = "d"
-                    elif d == "u":
-                        d = "r"
-                    elif d == "d":
-                        d = "l"
-                elif tile == "\\":
-                    if d == "r":
-                        d = "d"
-                    elif d == "l":
-                        d = "u"
-                    elif d == "u":
-                        d = "l"
-                    elif d == "d":
-                        d = "r"
-                elif tile == "|":
-                    if d == "r" or d == "l":
-                        d = "u"
-                        beams.append(("d", r + 1, c))
-                elif tile == "-":
-                    if d == "u" or d == "d":
-                        d = "l"
-                        beams.append(("r", r, c + 1))
+    starts = [
+        (str("r"), int(0), int(0)),
+        (str("r"), rows - 1, int(0)),
+        (str("d"), int(0), int(0)),
+        (str("d"), int(0), cols - 1),
+        (str("u"), rows - 1, int(0)),
+        (str("u"), rows - 1, cols - 1),
+        (str("l"), rows - 1, cols - 1),
+        (str("l"), int(0), cols - 1),
+    ]
+    max_tiles = 0
+    while len(starts) != 0:
+        beams = [starts.pop()]
+        energized_tiles = 0
+        visited = set()
+        tile_tracker = set()
+        while len(beams) != 0:
+            d, r, c = beams.pop()
+            while -1 < r < rows and -1 < c < cols and (d, r, c) not in visited:
+                # print(f"row: {r} col: {c} elem: {grid[r][c]} beams: {beams}")
+                visited.add((d, r, c))
+                if (r, c) not in tile_tracker:
+                    energized_tiles += 1
+                    tile_tracker.add((r, c))
+                if grid[r][c] in "/\\|-":
+                    # logic for adding beams and changing directions
+                    tile = grid[r][c]
+                    if tile == "/":
+                        if d == "r":
+                            d = "u"
+                        elif d == "l":
+                            d = "d"
+                        elif d == "u":
+                            d = "r"
+                        elif d == "d":
+                            d = "l"
+                    elif tile == "\\":
+                        if d == "r":
+                            d = "d"
+                        elif d == "l":
+                            d = "u"
+                        elif d == "u":
+                            d = "l"
+                        elif d == "d":
+                            d = "r"
+                    elif tile == "|":
+                        if d == "r" or d == "l":
+                            d = "u"
+                            beams.append(("d", r + 1, c))
+                    elif tile == "-":
+                        if d == "u" or d == "d":
+                            d = "l"
+                            beams.append(("r", r, c + 1))
 
-            if d == "r":
-                c += 1
-            elif d == "l":
-                c -= 1
-            elif d == "d":
-                r += 1
-            elif d == "u":
-                r -= 1
+                if d == "r":
+                    c += 1
+                elif d == "l":
+                    c -= 1
+                elif d == "d":
+                    r += 1
+                elif d == "u":
+                    r -= 1
+        print(energized_tiles)
+        max_tiles = max(max_tiles, energized_tiles)
 
-    return energized_tiles
+    return max_tiles
 
 
 def solution_helper(input_file: str) -> tuple:
@@ -65,7 +79,6 @@ def solution_helper(input_file: str) -> tuple:
     return find_energized_tiles(grid), 0
 
 
-
 def print_grid(grid: list) -> None:
     for row in grid:
         print(*row, sep=" ")
@@ -76,6 +89,7 @@ def solution(input_file: str) -> None:
     print(f"Solution P1 using '{input_file}': {answers[0]}")
     # print(f"Solution P2 using '{input_file}': {answers[1]}")
 
+
 if __name__ == "__main__":
     solution("test.txt")
-    solution("day_16_puzzle_input.txt")
+    # solution("day_16_puzzle_input.txt")
