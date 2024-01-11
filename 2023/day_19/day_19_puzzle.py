ac6ff374c5096595dac75b67fdc22d20d085abe0
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-11 18:15:27 -0500

    remove unecessary copying

--- 2023/day_19/day_19_puzzle.py
+++ 2023/day_19/day_19_puzzle.py
@@ -51,9 +51,7 @@ def get_accepted_parts_advanced(workflows: dict):
                         copy_dict[var] = combination_range_if_passed
                         stack.append((post, copy_dict))
                     if combination_range_if_failed[0] <= combination_range_if_failed[1]:
-                        copy_dict = dict(parts_dict)
-                        copy_dict[var] = combination_range_if_failed
-                        parts_dict = copy_dict
+                        parts_dict[var] = combination_range_if_failed
                 else:  # fallback
                     stack.append((rule_or_fallback, parts_dict))
 
