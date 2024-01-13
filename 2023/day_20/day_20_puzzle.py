13913ea1b4913af189c4f61606cd0d8f0dd5beff
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-13 01:38:35 -0500

    cleaner with classes

--- 2023/day_20/day_20_puzzle.py
+++ 2023/day_20/day_20_puzzle.py
@@ -1,6 +1,60 @@
 from queue import Queue
 
 
+class FF:
+    def __init__(self, id: str, outputs: list, state: bool = False):
+        self.id = id
+        self.outputs = outputs
+        self.state = state
+
+    def flip_and_send(self):
+        pulse_to_send = None
+        if not self.state:
+            # turn on, send high pulse
+            pulse_to_send = "h"
+        else:
+            # turn off, send low pulse
+            pulse_to_send = "l"
+
+        self.state = not self.state
+        return pulse_to_send
+
+    def __repr__(self):
+        return f"{self.id}: outputs={self.outputs}, state={self.state}"
+
+
+class CJ:
+    def __init__(self, id: str, outputs: list):
+        self.id = id
+        self.inputs = {}
+        self.outputs = outputs
+        self.pulses = [0, 0]
+
+    def update_inputs(self, key: str):
+        if key not in self.inputs:
+            # by default l is the remembered pulse
+            self.inputs[key] = "l"
+            self.pulses[0] += 1
+
+    def update_and_send(self, pulse_type: str, key: str):
+        if self.inputs[key] != pulse_type:
+            self.inputs[key] = pulse_type
+            if pulse_type == "l":
+                self.pulses[0] += 1
+                self.pulses[1] -= 1
+            else:
+                self.pulses[1] += 1
+                self.pulses[0] -= 1
+
+        if self.pulses[1] == len(self.inputs):
+            return "l"
+
+        return "h"
+
+    def __repr__(self):
+        return f"{self.id}: inputs={self.inputs}, outputs={self.outputs}, pulses={self.pulses}"
+
+
 def is_original_state(flip_flops: dict, conjuctions: dict) -> bool:
     for ff_v in flip_flops.values():
         # make sure all flip flops are off
@@ -15,7 +69,9 @@ def is_original_state(flip_flops: dict, conjuctions: dict) -> bool:
     return True
 
 
-def handle_broadcast_signals(broadcaster: list, flip_flops: dict, conjuctions: dict):
+def handle_signals(
+    broadcaster: list, flip_flops: dict[str, FF], conjuctions: dict[str, CJ]
+):
     total_pulse = {"l": 0, "h": 0}
     cycles = 0
     q = Queue()
@@ -39,46 +95,20 @@ def handle_broadcast_signals(broadcaster: list, flip_flops: dict, conjuctions: d
             if ff_key in flip_flops:
                 target_ff = flip_flops[ff_key]
                 if pulse_type == "l":
-                    # turn on, send high pulse
-                    pulse_to_send = pulse_type
-                    if not target_ff[1]:
-                        pulse_to_send = "h"
-                        target_ff[1] = True
-                    else:
-                        # turn off, send low pulse
-                        pulse_to_send = "l"
-                        target_ff[1] = False
-                    for next_module in target_ff[0]:
+                    pulse_to_send = target_ff.flip_and_send()
+                    for next_module in target_ff.outputs:
                         total_pulse[pulse_to_send] += 1
                         q.put((next_module, pulse_to_send, ff_key))
             elif c_key in conjuctions:
                 target_c = conjuctions[c_key]
-                if target_c[0][parent_key] != pulse_type:
-                    target_c[0][parent_key] = pulse_type
-                    if pulse_type == "l":
-                        # add to low , subtract from high
-                        target_c[1][0] += 1
-                        target_c[1][1] -= 1
-                    else:
-                        # add to high , subtract from low
-                        target_c[1][0] -= 1
-                        target_c[1][1] += 1
-                target_module_len = len(target_c[0])
-                pulse_to_send = pulse_type
-                if target_c[1][1] == target_module_len:
-                    pulse_to_send = "l"
-                else:
-                    pulse_to_send = "h"
+                pulse_to_send = target_c.update_and_send(pulse_type, parent_key)
 
-                for next_module in target_c[2]:
+                for next_module in target_c.outputs:
                     total_pulse[pulse_to_send] += 1
                     q.put((next_module, pulse_to_send, c_key))
 
-    return (total_pulse["l"] * total_pulse["h"])
+    return total_pulse["l"] * total_pulse["h"]
 
-
-def handle_signals(broadcaster: list, flip_flops: dict, conjuctions: dict):
-    return handle_broadcast_signals(broadcaster, flip_flops, conjuctions)
 
 
 def solution_helper(input_file: str):
@@ -88,31 +118,26 @@ def solution_helper(input_file: str):
     with open(input_file, "r") as file:
         for line in file:
             line = line.strip()
-            curr_split = line.split(" -> ")
+            id, outputs = line.split(" -> ")
             if line.startswith("broadcaster"):
-                broadcaster.extend(curr_split[1].split(", "))
+                broadcaster.extend(outputs.split(", "))
             elif line.startswith("%"):
-                flip_flops[curr_split[0]] = [curr_split[1].split(", "), False]
+                flip_flops[id] = FF(id, outputs.split(", "), False)
             elif line.startswith("&"):
-                module_split = curr_split[1].split(", ")
-                conjuctions[curr_split[0]] = [
-                    {},  # inputs
-                    [0, 0],
-                    [module for module in module_split],
-                ]
+                module_split = outputs.split(", ")
+                conjuctions[id] = CJ(id, module_split)
 
     for ff_key, ff_val in flip_flops.items():
-        for input in ff_val[0]:
+        for input in ff_val.outputs:
             c_key = f"&{input}"
             if c_key in conjuctions:
-                conjuctions[c_key][0][ff_key] = "l"
-                conjuctions[c_key][1][0] += 1
+                conjuctions[c_key].update_inputs(ff_key)
+
     for c_key, c_val in conjuctions.items():
-        for output in c_val[2]:
+        for output in c_val.outputs:
             n_c_key = f"&{output}"
             if n_c_key in conjuctions:
-                conjuctions[n_c_key][0][c_key] = "l"
-                conjuctions[n_c_key][1][0] += 1
+                conjuctions[n_c_key].update_inputs(c_key)
 
     return handle_signals(broadcaster, flip_flops, conjuctions)
 
@@ -128,6 +153,6 @@ if __name__ == "__main__":
         [
             "test.txt",
             "test2.txt",
-            "day_20_puzzle_input.txt"
+            "day_20_puzzle_input.txt",
         ]
     )
