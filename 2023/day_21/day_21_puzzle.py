5914302456660a7cff948655955f36ff252d4349
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-13 22:19:19 -0500

    part 1 done

--- 2023/day_21/day_21_puzzle.py
+++ 2023/day_21/day_21_puzzle.py
@@ -1,11 +1,46 @@
-def find_steps(start: tuple, grid: list):
-    print(f"{start[0]} {start[1]} have an S!")
-    print_grid(grid)
-    return 0
+from queue import Queue
+
+
+def find_steps(start: tuple, grid: list, max_steps: int):
+    total_plots = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    q = Queue()
+    q.put((start[0], start[1], int(0)))
+    visited = set()
+    dirs = {"n": (-1, 0), "s": (1, 0), "e": (0, -1), "w": (0, 1)}
+    while q.qsize() > 0:
+        r, c, curr_steps = q.get()
+
+        if (curr_steps, r, c) in visited or grid[r][c] == "#":
+            continue
+
+
+        visited.add((curr_steps, r, c))
+
+        if curr_steps == max_steps:
+            visited.add((r, c))
+            grid[r][c] = "O"
+            total_plots += 1
+
+        possible_moves = ["n", "s", "e", "w"]
+
+        for move in possible_moves:
+            d_r, d_c = dirs[move]
+            n_r = r + d_r
+            n_c = c + d_c
+
+            if -1 < n_r < rows and -1 < n_c < cols:
+                if curr_steps < max_steps:
+                    q.put((n_r, n_c, curr_steps + 1))
+
+    return total_plots
+
 
 def print_grid(grid: list) -> None:
     for row in grid:
         print(*row, sep=" ")
+
 
 def solution_helper(input_file: str):
     grid = []
@@ -23,7 +58,7 @@ def solution_helper(input_file: str):
             grid.append(list(line))
             curr_r += 1
 
-    return find_steps((s_r, s_c),grid)
+    return find_steps((s_r, s_c), grid, 64)
 
 
 def solution(inputs: list[str]) -> None:
@@ -36,6 +71,6 @@ if __name__ == "__main__":
     solution(
         [
             "test.txt",
-            # "day_21_puzzle_input.txt"
+            "day_21_puzzle_input.txt"
         ]
     )
