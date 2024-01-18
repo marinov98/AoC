6d2f4bc36616d9c2bddcafe5b373efa5d920ed34
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-17 20:24:30 -0500

    alternative way of doing part 2

--- 2022/elixir/aoc_2022/lib/day_1/day_1.ex
+++ 2022/elixir/aoc_2022/lib/day_1/day_1.ex
@@ -1,5 +1,5 @@
 defmodule Day1 do
-  @inputs ["lib/day_1/test.txt", "lib/day_1/day_1_input.txt"]
+  @inputs [ "lib/day_1/test.txt", "lib/day_1/day_1_input.txt" ]
 
   def solution() do
     @inputs
@@ -18,7 +18,7 @@ defmodule Day1 do
       |> Enum.sum()
     end)
 
-    {part1(input), part2(input)}
+    {{"Part 1 using '#{input_file}'", part1(input)}, {"Part 2 using '#{input_file}'", part2(input)}}
   end
 
   def part1(input) do
@@ -28,8 +28,18 @@ defmodule Day1 do
 
   def part2(input) do
     input
-    |> Enum.sort(&(&1 >= &2))
-    |> Enum.take(3)
-    |> Enum.sum
+    # alternative lazy method with sorting and taking the first 3
+    #    |> Enum.sort(&(&1 >= &2))
+    #    |> Enum.take(3)
+    #    |> Enum.sum
+    |> Enum.reduce({0, 0, 0}, fn cals, acc ->
+                    cond do
+                      cals > elem(acc, 0) -> {cals, elem(acc, 0), elem(acc, 1)}
+                      cals > elem(acc, 1) -> {elem(acc, 0), cals, elem(acc, 1)}
+                      cals > elem(acc, 2) -> {elem(acc, 0), elem(acc, 1), cals}
+                      true -> acc
+                    end
+                  end)
+    |> Tuple.sum()
   end
 end
