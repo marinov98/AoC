554d23615b7fef49853d7cf5d515990c1e0cb81a
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-18 20:06:58 -0500

    both parts done

--- 2022/elixir/aoc_2022/lib/day_4/day4.ex
+++ 2022/elixir/aoc_2022/lib/day_4/day4.ex
@@ -7,24 +7,31 @@ defmodule Day4 do
   end
 
   def solution_helper(input_file) do
-    input = input_file |> File.read!() |> String.trim() |> String.split("\n")
-    {part1(input), 0}
+    input =
+      input_file
+      |> File.read!()
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(fn curr_str ->
+        curr_str
+        |> String.trim()
+        |> String.split(",")
+      end)
+
+    {part1(input), part2(input)}
   end
 
   def part1(input) do
     input
-    |> Enum.map(fn curr_str ->
-      curr_str
-      |> String.trim()
-      |> String.split(",")
-    end)
-    # |> Enum.filter(&part1_filter(List.first(&1), List.last(&1)))
     |> Enum.reduce(0, &(part1_utility(&1) + &2))
   end
 
   defp part1_utility(ranges) do
-    [s1 | e1] = List.first(ranges) |> String.split("-") |> Enum.map(&String.to_integer/1)
-    [s2 | e2] = List.last(ranges) |> String.split("-") |> Enum.map(&String.to_integer/1)
+    [s1 | rest] = List.first(ranges) |> String.split("-") |> Enum.map(&String.to_integer/1)
+    [s2 | rest2] = List.last(ranges) |> String.split("-") |> Enum.map(&String.to_integer/1)
+    e1 = List.first(rest)
+    e2 = List.first(rest2)
+
     cond do
       s1 <= s2 and e1 >= e2 -> 1
       s2 <= s1 and e2 >= e1 -> 1
@@ -32,17 +39,21 @@ defmodule Day4 do
     end
   end
 
-  defp part1_filter(left, right) do
-    s1 = String.to_integer(String.first(left))
-    e1 = String.to_integer(String.last(left))
+  defp part2(input) do
+    input
+    |> Enum.reduce(0, &(part2_utility(&1) + &2))
+  end
 
-    s2 = String.to_integer(String.first(right))
-    e2 = String.to_integer(String.last(right))
+  defp part2_utility(ranges) do
+    [s1 | rest] = List.first(ranges) |> String.split("-") |> Enum.map(&String.to_integer/1)
+    [s2 | rest2] = List.last(ranges) |> String.split("-") |> Enum.map(&String.to_integer/1)
+    e1 = List.first(rest)
+    e2 = List.first(rest2)
 
     cond do
-      s1 <= s2 and e1 >= e2 -> true
-      s2 <= s1 and e2 >= e1 -> true
-      true -> false
+      s1 <= s2 and s2 <= e1 -> 1
+      s2 <= s1 and s1 <= e2 -> 1
+      true -> 0
     end
   end
 end
