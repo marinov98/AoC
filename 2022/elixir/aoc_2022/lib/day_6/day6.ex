b8d099c06aac0b8c77ac600ce9624290cf4ee9ef
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-20 10:17:17 -0500

    finish day 6

--- 2022/elixir/aoc_2022/lib/day_6/day6.ex
+++ 2022/elixir/aoc_2022/lib/day_6/day6.ex
@@ -1,5 +1,6 @@
 defmodule Day6 do
   @inputs ["lib/day_6/test.txt", "lib/day_6/day6input.txt"]
+  # @inputs ["lib/day_6/test.txt"]
 
   def solution() do
     @inputs
@@ -11,7 +12,56 @@ defmodule Day6 do
       input_file
       |> File.read!()
       |> String.trim()
-    # {part1(input), part2(input)}
+      |> String.split("\n")
+
+    {part1(input), part2(input)}
   end
-  
+
+  def part1(input) do
+    input
+    |> Enum.map(&part1_helper/1)
+  end
+
+  def part1_helper(input, left \\ 0, right \\ 0, tracker \\ []) do
+    cond do
+      length(tracker) == 4 and length(Enum.uniq(tracker)) == 4 ->
+        right
+
+      true ->
+        case right - left > 3 do
+          true ->
+            [_ | tracker] = tracker
+            tracker = List.insert_at(tracker, -1, String.at(input, right))
+            part1_helper(input, left + 1, right + 1, tracker)
+
+          false ->
+            tracker = List.insert_at(tracker, -1, String.at(input, right))
+            part1_helper(input, left, right + 1, tracker)
+        end
+    end
+  end
+
+  def part2(input) do
+    input
+    |> Enum.map(&part2_helper/1)
+  end
+
+  def part2_helper(input, left \\ 0, right \\ 0, tracker \\ []) do
+    cond do
+      length(tracker) == 14 and length(Enum.uniq(tracker)) == 14 ->
+        right
+
+      true ->
+        case right - left > 13 do
+          true ->
+            [_ | tracker] = tracker
+            tracker = List.insert_at(tracker, -1, String.at(input, right))
+            part2_helper(input, left + 1, right + 1, tracker)
+
+          false ->
+            tracker = List.insert_at(tracker, -1, String.at(input, right))
+            part2_helper(input, left, right + 1, tracker)
+        end
+    end
+  end
 end
