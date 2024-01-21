898adb667517ba43e6f71c6d05315bfadf93cad2
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-21 15:49:03 -0500

    complete both parts

--- 2022/elixir/aoc_2022/lib/day_8/day8.ex
+++ 2022/elixir/aoc_2022/lib/day_8/day8.ex
@@ -14,7 +14,7 @@ defmodule Day8 do
       |> String.split("\n", trim: true)
       |> Enum.map(fn elem -> elem |> String.graphemes() |> Enum.map(&String.to_integer/1) end)
 
-    {part1(input), 0}
+    {part1(input), part2(input)}
   end
 
   def part1(grid) do
@@ -33,41 +33,41 @@ defmodule Day8 do
     |> elem(0)
   end
 
-  defp visible?(grid, r, c, rows, cols, visibles) do
+  defp visible?(grid, r, c, rows, cols, dp) do
     curr = get_in(grid, [Access.at(r), Access.at(c)])
 
     cond do
       r == 0 or c == 0 or r == rows - 1 or c == cols - 1 ->
-        visibles = MapSet.put(visibles, {r, c, "e"})
-        {true, visibles}
+        dp = MapSet.put(dp, {r, c, "e"})
+        {true, dp}
 
-      is_visible_dir?(grid, r, c - 1, curr, visibles, "l") ->
-        visibles = MapSet.put(visibles, {r, c, "l"})
-        {true, visibles}
+      is_visible_dir?(grid, r, c - 1, curr, dp, "l") ->
+        dp = MapSet.put(dp, {r, c, "l"})
+        {true, dp}
 
-      is_visible_dir?(grid, r, c + 1, curr, visibles, "r") ->
-        visibles = MapSet.put(visibles, {r, c, "r"})
-        {true, visibles}
+      is_visible_dir?(grid, r, c + 1, curr, dp, "r") ->
+        dp = MapSet.put(dp, {r, c, "r"})
+        {true, dp}
 
-      is_visible_dir?(grid, r - 1, c, curr, visibles, "u") ->
-        visibles = MapSet.put(visibles, {r, c, "u"})
-        {true, visibles}
+      is_visible_dir?(grid, r - 1, c, curr, dp, "u") ->
+        dp = MapSet.put(dp, {r, c, "u"})
+        {true, dp}
 
-      is_visible_dir?(grid, r + 1, c, curr, visibles, "d") ->
-        visibles = MapSet.put(visibles, {r, c, "d"})
-        {true, visibles}
+      is_visible_dir?(grid, r + 1, c, curr, dp, "d") ->
+        dp = MapSet.put(dp, {r, c, "d"})
+        {true, dp}
 
       true ->
-        {false, visibles}
+        {false, dp}
     end
   end
 
-  defp is_visible_dir?(grid, r, c, val, visibles, dir) do
+  defp is_visible_dir?(grid, r, c, val, dp, dir) do
     curr = get_in(grid, [Access.at(r), Access.at(c)])
 
     cond do
       val > curr and
-          (MapSet.member?(visibles, {r, c, dir}) or MapSet.member?(visibles, {r, c, "e"})) ->
+          (MapSet.member?(dp, {r, c, dir}) or MapSet.member?(dp, {r, c, "e"})) ->
         true
 
       curr == nil ->
@@ -76,20 +76,86 @@ defmodule Day8 do
       val > curr ->
         case dir do
           "l" ->
-            is_visible_dir?(grid, r, c - 1, val, visibles, dir)
+            is_visible_dir?(grid, r, c - 1, val, dp, dir)
 
           "r" ->
-            is_visible_dir?(grid, r, c + 1, val, visibles, dir)
+            is_visible_dir?(grid, r, c + 1, val, dp, dir)
 
           "u" ->
-            is_visible_dir?(grid, r - 1, c, val, visibles, dir)
+            is_visible_dir?(grid, r - 1, c, val, dp, dir)
 
           "d" ->
-            is_visible_dir?(grid, r + 1, c, val, visibles, dir)
+            is_visible_dir?(grid, r + 1, c, val, dp, dir)
         end
 
       true ->
         false
+    end
+  end
+
+  def part2(grid) do
+    rows = length(grid)
+    cols = hd(grid) |> length
+
+    dp =
+      for row_edge <- 0..(rows - 1), reduce: %{} do
+        acc ->
+          acc = Map.put(acc, {row_edge, 0, "e"}, 0)
+          Map.put(acc, {row_edge, cols - 1, "e"}, 0)
+      end
+
+    dp =
+      for col_edge <- 0..(cols - 1), reduce: dp do
+        acc ->
+          acc = Map.put(acc, {0, col_edge, "e"}, 0)
+          Map.put(acc, {rows - 1, col_edge, "e"}, 0)
+      end
+
+    for r <- 1..(rows - 2), c <- 1..(cols - 2), reduce: 0 do
+      max_score ->
+        curr_score = get_scenic_score(grid, r, c, rows, cols)
+
+        case curr_score > max_score do
+          true -> curr_score
+          false -> max_score
+        end
+    end
+  end
+
+  def get_scenic_score(grid, r, c, rows, cols) do
+    curr = get_in(grid, [Access.at(r), Access.at(c)])
+    view_dist_l = calc_dist(grid, r, c - 1, curr, "l")
+    view_dist_r = calc_dist(grid, r, c + 1, curr, "r")
+    view_dist_u = calc_dist(grid, r - 1, c, curr, "u")
+    view_dist_d = calc_dist(grid, r + 1, c, curr, "d")
+
+    view_dist_l * view_dist_r * view_dist_u * view_dist_d
+  end
+
+  def calc_dist(grid, r, c, val, dir) do
+    curr = get_in(grid, [Access.at(r), Access.at(c)])
+
+    cond do
+      curr == nil or r < 0 or c < 0 ->
+        0
+
+      curr >= val ->
+        1
+
+      curr < val ->
+        case dir do
+          "l" ->
+            1 + calc_dist(grid, r, c - 1, val, dir)
+
+          "r" ->
+            1 + calc_dist(grid, r, c + 1, val, dir)
+
+          "u" ->
+            1 + calc_dist(grid, r - 1, c, val, dir)
+
+          "d" ->
+            1 + calc_dist(grid, r + 1, c, val, dir)
+        end
     end
   end
 end
