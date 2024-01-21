9bc72089b1c2bfcc8f96148500f19629c6a60a39
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-20 20:28:36 -0500

    good progress on part 1

--- 2022/elixir/aoc_2022/lib/day_7/day7.ex
+++ 2022/elixir/aoc_2022/lib/day_7/day7.ex
@@ -21,8 +21,7 @@ defmodule Day7 do
     |> part1_utility(List.first(input))
   end
 
-  def part1_utility(input, curr_line, parent_tracker \\ %{}, curr_dir \\ "", size_tracker \\ %{}, children_tracker \\ %{}) do
-    IO.puts(curr_line)
+  def part1_utility(input, curr_line, parent_tracker \\ %{}, curr_dir \\ "", size_tracker \\ %{}, children_tracker \\ %{}, file_name_tracker \\ %{}) do
     case input do
       [] -> {size_tracker, children_tracker}
       _ ->
@@ -38,28 +37,28 @@ defmodule Day7 do
                 "" -> # should be first command
                   children_tracker = Map.put(children_tracker, next_dir, MapSet.new())
                   size_tracker = Map.put(size_tracker, next_dir, 0)
-                  parent_tracker = Map.put(parent_tracker, next_dir, curr_dir)
+                  # parent_tracker = Map.put(parent_tracker, next_dir, curr_dir)
                   [_ | next_lines ] = input
-                  part1_utility(next_lines, List.first(next_lines), parent_tracker, next_dir, size_tracker, children_tracker)
+                  part1_utility(next_lines, List.first(next_lines), parent_tracker, next_dir, size_tracker, children_tracker, file_name_tracker)
                 _ -> 
                     case next_dir do
                       ".." -> 
                           prev_dir = Map.get(parent_tracker, curr_dir)
-                          part1_utility(next_lines, List.first(next_lines), parent_tracker, prev_dir, size_tracker, children_tracker)
+                          part1_utility(next_lines, List.first(next_lines), parent_tracker, prev_dir, size_tracker, children_tracker, file_name_tracker)
                       _ ->
                       case Map.get(children_tracker, next_dir) do
                         nil -> 
                           children_tracker = Map.put(children_tracker, next_dir, MapSet.new())
                           size_tracker = Map.put(size_tracker, next_dir, 0)
                           parent_tracker = Map.put(parent_tracker, next_dir, curr_dir)
-                          part1_utility(next_lines, List.first(next_lines), parent_tracker, next_dir, size_tracker, children_tracker)
+                          part1_utility(next_lines, List.first(next_lines), parent_tracker, next_dir, size_tracker, children_tracker, file_name_tracker)
                         _ -> 
-                          part1_utility(next_lines, List.first(next_lines), parent_tracker, next_dir, size_tracker, children_tracker)
+                          part1_utility(next_lines, List.first(next_lines), parent_tracker, next_dir, size_tracker, children_tracker, file_name_tracker)
                       end
                     end
               end
               "ls" -> 
-                  part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker)
+                  part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
           end
         "dir" -> # dir
           [_ | rest ] = split
@@ -69,14 +68,24 @@ defmodule Day7 do
               false ->
                 curr_dir_children = MapSet.put(curr_dir_children, next_dir)
                 children_tracker = Map.put(children_tracker, curr_dir, curr_dir_children)
-                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker)
+                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
               true ->
-                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker)
+                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
           end
         _ -> # size
-            val = String.to_integer(List.first(split))
-            size_tracker = Map.update(size_tracker, curr_dir, val, &(&1 + val))
-            part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker)
+            file_name = List.last(split)
+            curr_files = Map.get(file_name_tracker, curr_dir, MapSet.new())
+            case  MapSet.member?(curr_files, file_name) do
+              false -> 
+                val = String.to_integer(List.first(split))
+                curr_files = MapSet.put(curr_files, file_name)
+                file_name_tracker = Map.put(file_name_tracker, curr_dir, curr_files)
+                size_tracker = Map.update(size_tracker, curr_dir, val, &(&1 + val))
+                size_tracker = update_parents(parent_tracker, size_tracker, curr_dir, val)
+                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
+              true ->
+                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
+            end
       end
     end
   end
@@ -85,8 +94,14 @@ defmodule Day7 do
     input
   end
 
-  def calc_total_size(trackers_tuple) do
-    {size_tracker, children_tracker} = trackers_tuple
+  def update_parents(parent_tracker, size_tracker, curr, val) do
+    parent_dir = Map.get(parent_tracker, curr)
+    case parent_dir do
+      nil ->
+        size_tracker
+      _ -> 
+        size_tracker = Map.update(size_tracker, parent_dir, val, &(&1 + val))
+        update_parents(parent_tracker, size_tracker, parent_dir, val)
+    end
   end
-
 end
