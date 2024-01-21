ec6d502441bd852268bc9442773ec3fd5fbd1d6b
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-21 00:02:37 -0500

    improvement but still not right

--- 2022/elixir/aoc_2022/lib/day_7/day7.ex
+++ 2022/elixir/aoc_2022/lib/day_7/day7.ex
@@ -1,6 +1,6 @@
 defmodule Day7 do
-  # @inputs ["lib/day_7/test.txt", "lib/day_7/day7input.txt"]
-  @inputs ["lib/day_7/test.txt"]
+  @inputs ["lib/day_7/test.txt", "lib/day_7/day7input.txt"]
+  # @inputs ["lib/day_7/test.txt"]
 
   def solution() do
     @inputs
@@ -19,59 +19,49 @@ defmodule Day7 do
   def part1(input) do
     input
     |> part1_utility(List.first(input))
+    |> Enum.flat_map(fn {_, v} -> 
+      case v < 100001 do
+        true -> [v]
+        false -> []
+      end
+    end)
+    |> Enum.sum()
   end
 
-  def part1_utility(input, curr_line, parent_tracker \\ %{}, curr_dir \\ "", size_tracker \\ %{}, children_tracker \\ %{}, file_name_tracker \\ %{}) do
+  def part1_utility(input, curr_line, path \\ [], curr_dir \\ "", size_tracker \\ %{}, children_tracker \\ %{}, file_name_tracker \\ %{}) do
     case input do
-      [] -> {size_tracker, children_tracker}
+      [] -> 
+        # {size_tracker, children_tracker}
+        size_tracker
       _ ->
       split = String.split(curr_line, " ")
       [_ | next_lines ] = input
       case hd(split) do
         "$" -> # command
           [_ | rest ] = split
-          next_dir = List.last(rest)
           case List.first(rest) do
             "cd" -> # going into new directory
+              next_dir = List.last(rest)
               case curr_dir do
                 "" -> # should be first command
                   children_tracker = Map.put(children_tracker, next_dir, MapSet.new())
-                  size_tracker = Map.put(size_tracker, next_dir, 0)
-                  # parent_tracker = Map.put(parent_tracker, next_dir, curr_dir)
-                  [_ | next_lines ] = input
-                  part1_utility(next_lines, List.first(next_lines), parent_tracker, next_dir, size_tracker, children_tracker, file_name_tracker)
+                  path = [next_dir]
+                  size_tracker = Map.put(size_tracker, Enum.join(path), 0)
+                  part1_utility(next_lines, List.first(next_lines), path, next_dir, size_tracker, children_tracker, file_name_tracker)
                 _ -> 
                     case next_dir do
                       ".." -> 
-                          prev_dir = Map.get(parent_tracker, curr_dir)
-                          part1_utility(next_lines, List.first(next_lines), parent_tracker, prev_dir, size_tracker, children_tracker, file_name_tracker)
+                          [prev_dir | remaining_path] = path
+                          part1_utility(next_lines, List.first(next_lines), remaining_path, prev_dir, size_tracker, children_tracker, file_name_tracker)
                       _ ->
-                      case Map.get(children_tracker, next_dir) do
-                        nil -> 
-                          children_tracker = Map.put(children_tracker, next_dir, MapSet.new())
-                          size_tracker = Map.put(size_tracker, next_dir, 0)
-                          parent_tracker = Map.put(parent_tracker, next_dir, curr_dir)
-                          part1_utility(next_lines, List.first(next_lines), parent_tracker, next_dir, size_tracker, children_tracker, file_name_tracker)
-                        _ -> 
-                          part1_utility(next_lines, List.first(next_lines), parent_tracker, next_dir, size_tracker, children_tracker, file_name_tracker)
-                      end
+                        part1_utility(next_lines, List.first(next_lines), [next_dir | path], next_dir, size_tracker, children_tracker, file_name_tracker)
                     end
               end
               "ls" -> 
-                  part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
+                  part1_utility(next_lines, List.first(next_lines), path, curr_dir, size_tracker, children_tracker, file_name_tracker)
           end
         "dir" -> # dir
-          [_ | rest ] = split
-          next_dir = List.last(rest)
-          curr_dir_children = Map.get(children_tracker, curr_dir)
-          case MapSet.member?(curr_dir_children, next_dir) do
-              false ->
-                curr_dir_children = MapSet.put(curr_dir_children, next_dir)
-                children_tracker = Map.put(children_tracker, curr_dir, curr_dir_children)
-                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
-              true ->
-                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
-          end
+            part1_utility(next_lines, List.first(next_lines), path, curr_dir, size_tracker, children_tracker, file_name_tracker)
         _ -> # size
             file_name = List.last(split)
             curr_files = Map.get(file_name_tracker, curr_dir, MapSet.new())
@@ -80,11 +70,11 @@ defmodule Day7 do
                 val = String.to_integer(List.first(split))
                 curr_files = MapSet.put(curr_files, file_name)
                 file_name_tracker = Map.put(file_name_tracker, curr_dir, curr_files)
-                size_tracker = Map.update(size_tracker, curr_dir, val, &(&1 + val))
-                size_tracker = update_parents(parent_tracker, size_tracker, curr_dir, val)
-                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
+                size_tracker = Map.update(size_tracker, Enum.join(path), val, &(&1 + val))
+                size_tracker = update_parents(path, size_tracker, val)
+                part1_utility(next_lines, List.first(next_lines), path, curr_dir, size_tracker, children_tracker, file_name_tracker)
               true ->
-                part1_utility(next_lines, List.first(next_lines), parent_tracker, curr_dir, size_tracker, children_tracker, file_name_tracker)
+                part1_utility(next_lines, List.first(next_lines), path, curr_dir, size_tracker, children_tracker, file_name_tracker)
             end
       end
     end
@@ -92,16 +82,18 @@ defmodule Day7 do
 
   def part2(input) do
     input
+    0
   end
 
-  def update_parents(parent_tracker, size_tracker, curr, val) do
-    parent_dir = Map.get(parent_tracker, curr)
-    case parent_dir do
-      nil ->
+  def update_parents(path, size_tracker, val) do
+    [_ | rest] = path
+    case rest do
+       [] -> 
         size_tracker
-      _ -> 
-        size_tracker = Map.update(size_tracker, parent_dir, val, &(&1 + val))
-        update_parents(parent_tracker, size_tracker, parent_dir, val)
+       _ ->
+        size_tracker = Map.update(size_tracker, Enum.join(rest), val, &(&1 + val))
     end
   end
+
+
 end
