a28682b7b20116cae2d43db395a54424d7f22b12
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-23 18:59:44 -0500

    finish day 10 part 1

--- 2022/elixir/aoc_2022/lib/day_10/day10.ex
+++ 2022/elixir/aoc_2022/lib/day_10/day10.ex
@@ -1,6 +1,8 @@
 defmodule Day10 do
-  @inputs ["lib/day_10/test.txt", "lib/day_10/day10input.txt"]
+  @inputs ["lib/day_10/test2.txt", "lib/day_10/day10input.txt"]
   # @inputs ["lib/day_10/test.txt"]
+  # @inputs ["lib/day_10/test2.txt"]
+  @targets MapSet.new([20, 60, 100, 140, 180, 220])
 
   def solution() do
     @inputs
@@ -11,5 +13,50 @@ defmodule Day10 do
     input =
       input_file
       |> File.read!()
+      |> String.split("\n", trim: true)
+
+    {part1(input), 0}
+  end
+
+  def part1(input) do
+    input
+    |> Enum.map(&String.split/1)
+    |> simulation
+    |> Enum.sum
+  end
+
+  defp simulation(instructions, x \\ 1, cycle \\ 1, signal_str_sum \\ []) do
+    case instructions do
+      [] ->
+        signal_str_sum
+
+      _ ->
+        [curr_instruction | rest] = instructions
+
+        case length(curr_instruction) do
+          1 ->
+            case MapSet.member?(@targets, cycle + 1) do
+              false ->
+                simulation(rest, x, cycle + 1, signal_str_sum)
+
+              true ->
+                simulation(rest, x, cycle + 1, [x * (cycle + 1) | signal_str_sum])
+            end
+
+          2 ->
+            val = curr_instruction |> List.last() |> String.to_integer()
+
+            cond do
+              MapSet.member?(@targets, cycle + 1) ->
+                simulation(rest, x + val, cycle + 2, [x * (cycle + 1) | signal_str_sum])
+
+              MapSet.member?(@targets, cycle + 2) ->
+                simulation(rest, x + val, cycle + 2, [(x + val) * (cycle + 2) | signal_str_sum])
+
+              true ->
+                simulation(rest, x + val, cycle + 2, signal_str_sum)
+            end
+        end
+    end
   end
 end
