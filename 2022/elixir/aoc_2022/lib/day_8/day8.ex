c7bf6dddbdfc4bc665328ea86f048c374b76686e
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-23 18:02:35 -0500

    experiment with dp but copying too expensive probably

--- 2022/elixir/aoc_2022/lib/day_8/day8.ex
+++ 2022/elixir/aoc_2022/lib/day_8/day8.ex
@@ -104,42 +104,53 @@ defmodule Day8 do
     rows = length(grid)
     cols = hd(grid) |> length
 
-    for r <- 1..(rows - 2), c <- 1..(cols - 2), reduce: 0 do
-      curr_max_score ->
-        max(curr_max_score, get_scenic_score(grid, r, c))
+    for r <- 1..(rows - 2), c <- 1..(cols - 2), reduce: {0, %{}} do
+      {curr_max_score, dp} ->
+        {new_score, dp} = get_scenic_score(grid, r, c, dp)
+        {max(curr_max_score, new_score), dp}
     end
+    |> elem(0)
   end
 
-  def get_scenic_score(grid, r, c) do
+  defp get_scenic_score(grid, r, c, dp) do
     curr = get_in(grid, [Access.at(r), Access.at(c)])
+    view_dist_l = calc_dist(grid, r, c - 1, curr, "l", dp)
+    dp = Map.put(dp, {r, c, "l"}, view_dist_l)
+    view_dist_r = calc_dist(grid, r, c + 1, curr, "r", dp)
+    dp = Map.put(dp, {r, c, "r"}, view_dist_r)
+    view_dist_u = calc_dist(grid, r - 1, c, curr, "u", dp)
+    dp = Map.put(dp, {r, c, "u"}, view_dist_u)
+    view_dist_d = calc_dist(grid, r + 1, c, curr, "d", dp)
+    dp = Map.put(dp, {r, c, "d"}, view_dist_d)
 
-    calc_dist(grid, r, c - 1, curr, "l") * calc_dist(grid, r, c + 1, curr, "r") *
-      calc_dist(grid, r - 1, c, curr, "u") * calc_dist(grid, r + 1, c, curr, "d")
+    {view_dist_l * view_dist_r * view_dist_d * view_dist_u, dp}
   end
 
-  def calc_dist(grid, r, c, val, dir) do
+  defp calc_dist(grid, r, c, val, dir, dp) do
     curr = get_in(grid, [Access.at(r), Access.at(c)])
 
     cond do
-      curr == nil or r < 0 or c < 0 ->
+      curr == nil ->
         0
 
-      curr >= val ->
+      curr >= val or r == 0 or c == 0 or r == length(grid) - 1 or c == length(hd(grid)) - 1 ->
         1
 
       curr < val ->
+        pre_computed_dist = Map.get(dp, {r, c, dir}, 1)
+
         case dir do
           "l" ->
-            1 + calc_dist(grid, r, c - 1, val, dir)
+            pre_computed_dist + calc_dist(grid, r, c - pre_computed_dist, val, dir, dp)
 
           "r" ->
-            1 + calc_dist(grid, r, c + 1, val, dir)
+            pre_computed_dist + calc_dist(grid, r, c + pre_computed_dist, val, dir, dp)
 
           "u" ->
-            1 + calc_dist(grid, r - 1, c, val, dir)
+            pre_computed_dist + calc_dist(grid, r - pre_computed_dist, c, val, dir, dp)
 
           "d" ->
-            1 + calc_dist(grid, r + 1, c, val, dir)
+            pre_computed_dist + calc_dist(grid, r + pre_computed_dist, c, val, dir, dp)
         end
     end
   end
