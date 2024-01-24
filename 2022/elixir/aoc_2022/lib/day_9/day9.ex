be3215e08a42da42c4964ceb91af592c973a1df0
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-24 17:04:16 -0500

    proper naming and prepare for part 2

--- 2022/elixir/aoc_2022/lib/day_9/day9.ex
+++ 2022/elixir/aoc_2022/lib/day_9/day9.ex
@@ -1,6 +1,7 @@
 defmodule Day9 do
   @inputs ["lib/day_9/test.txt", "lib/day_9/day9input.txt"]
   # @inputs ["lib/day_9/test.txt"]
+  # @inputs ["lib/day_9/test2.txt"]
 
   def solution() do
     @inputs
@@ -16,7 +17,7 @@ defmodule Day9 do
     {part1(input), 0}
   end
 
-  def part1(input, h_r \\ 0, h_c \\ 0, t_r \\ 0, t_c \\ 0, tracker \\ MapSet.new([{0, 0}])) do
+  def part1(input, h_r \\ 0, h_c \\ 0, t_r \\ 0, t_c \\ 0, tracker \\ MapSet.new()) do
     case Enum.empty?(input) do
       true ->
         tracker |> MapSet.size()
@@ -71,6 +72,7 @@ defmodule Day9 do
     cd = h_c - t_c
     r_d = abs(rd)
     c_d = abs(cd)
+
     cond do
       # no need to change
       r_d < 2 and c_d < 2 ->
@@ -102,6 +104,63 @@ defmodule Day9 do
 
       # Diagonals
       r_d > 1 or c_d > 1 ->
+        cond do
+          # tail above head and to the left
+          rd > 0 and cd > 0 ->
+            {t_r + 1, t_c + 1}
+
+          # tail below head and to the right
+          rd < 0 and cd < 0 ->
+            {t_r - 1, t_c - 1}
+
+          # tail above head and to the right
+          rd > 0 and cd < 0 ->
+            {t_r + 1, t_c - 1}
+
+          # tail below head and to the left
+          rd < 0 and cd > 0 ->
+            {t_r - 1, t_c + 1}
+        end
+    end
+  end
+
+  defp fix_tail_alt(h_r, h_c, t_r, t_c, knots \\ 2) do
+    rd = h_r - t_r
+    cd = h_c - t_c
+    r_d = abs(rd)
+    c_d = abs(cd)
+
+    cond do
+      # no need to change
+      r_d < knots and c_d < knots ->
+        {t_r, t_c}
+
+      # h is above or below tail
+      r_d > knots - 1 and c_d == 0 ->
+        cond do
+          # tail below head
+          rd < 0 ->
+            {t_r - 1, t_c}
+
+          # tail above head
+          true ->
+            {t_r + 1, t_c}
+        end
+
+      # h is to the left or right of tail
+      c_d > knots - 1 and r_d == 0 ->
+        cond do
+          # tail to the right of head
+          cd < 0 ->
+            {t_r, t_c - 1}
+
+          # tail to the left of head
+          true ->
+            {t_r, t_c + 1}
+        end
+
+      # Diagonals
+      r_d > knots - 1 or c_d > knots - 1 ->
         cond do
           # tail above head and to the left
           rd > 0 and cd > 0 ->
