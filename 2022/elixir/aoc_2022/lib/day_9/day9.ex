dbac1263d7bdd832640176c33f87e8e19a5a2e4d
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-24 00:34:57 -0500

    day 9 part 1 finished

--- 2022/elixir/aoc_2022/lib/day_9/day9.ex
+++ 2022/elixir/aoc_2022/lib/day_9/day9.ex
@@ -11,5 +11,114 @@ defmodule Day9 do
     input =
       input_file
       |> File.read!()
+      |> String.split("\n", trim: true)
+
+    {part1(input), 0}
+  end
+
+  def part1(input, h_r \\ 0, h_c \\ 0, t_r \\ 0, t_c \\ 0, tracker \\ MapSet.new([{0, 0}])) do
+    case Enum.empty?(input) do
+      true ->
+        tracker |> MapSet.size()
+
+      false ->
+        [curr | rest] = input
+        curr_dir_split = String.split(curr, " ")
+        curr_dir = curr_dir_split |> hd
+        curr_moves = curr_dir_split |> List.last() |> String.to_integer()
+
+        {h_r, h_c, t_r, t_c, tracker} =
+          part1_helper(curr_dir, curr_moves, h_r, h_c, t_r, t_c, tracker)
+
+        part1(rest, h_r, h_c, t_r, t_c, tracker)
+    end
+  end
+
+  defp part1_helper(dir, moves, h_r, h_c, t_r, t_c, tracker) do
+    tracker = MapSet.put(tracker, {t_r, t_c})
+
+    case moves do
+      0 ->
+        {h_r, h_c, t_r, t_c, tracker}
+
+      _ ->
+        case dir do
+          "R" ->
+            h_c = h_c + 1
+            {t_r, t_c} = fix_tail(h_r, h_c, t_r, t_c)
+            part1_helper(dir, moves - 1, h_r, h_c, t_r, t_c, tracker)
+
+          "L" ->
+            h_c = h_c - 1
+            {t_r, t_c} = fix_tail(h_r, h_c, t_r, t_c)
+            part1_helper(dir, moves - 1, h_r, h_c, t_r, t_c, tracker)
+
+          "D" ->
+            h_r = h_r - 1
+            {t_r, t_c} = fix_tail(h_r, h_c, t_r, t_c)
+            part1_helper(dir, moves - 1, h_r, h_c, t_r, t_c, tracker)
+
+          "U" ->
+            h_r = h_r + 1
+            {t_r, t_c} = fix_tail(h_r, h_c, t_r, t_c)
+            part1_helper(dir, moves - 1, h_r, h_c, t_r, t_c, tracker)
+        end
+    end
+  end
+
+  defp fix_tail(h_r, h_c, t_r, t_c) do
+    rd = h_r - t_r
+    cd = h_c - t_c
+    r_d = abs(rd)
+    c_d = abs(cd)
+    cond do
+      # no need to change
+      r_d < 2 and c_d < 2 ->
+        {t_r, t_c}
+
+      # h is above or below tail
+      r_d > 1 and c_d == 0 ->
+        cond do
+          # tail below head
+          rd < 0 ->
+            {t_r - 1, t_c}
+
+          # tail above head
+          true ->
+            {t_r + 1, t_c}
+        end
+
+      # h is to the left or right of tail
+      c_d > 1 and r_d == 0 ->
+        cond do
+          # tail to the right of head
+          cd < 0 ->
+            {t_r, t_c - 1}
+
+          # tail to the left of head
+          true ->
+            {t_r, t_c + 1}
+        end
+
+      # Diagonals
+      r_d > 1 or c_d > 1 ->
+        cond do
+          # tail above head and to the left
+          rd > 0 and cd > 0 ->
+            {t_r + 1, t_c + 1}
+
+          # tail below head and to the right
+          rd < 0 and cd < 0 ->
+            {t_r - 1, t_c - 1}
+
+          # tail above head and to the right
+          rd > 0 and cd < 0 ->
+            {t_r + 1, t_c - 1}
+
+          # tail below head and to the left
+          rd < 0 and cd > 0 ->
+            {t_r - 1, t_c + 1}
+        end
+    end
   end
 end
