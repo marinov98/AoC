538e55ed3f188f1a4d230be7df8486ea0e673c9d
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-24 23:57:19 -0500

    cleanup day 1 refactoring

--- 2022/elixir/aoc_2022/lib/day_1/day1.ex
+++ 2022/elixir/aoc_2022/lib/day_1/day1.ex
@@ -18,34 +18,13 @@ defmodule Day1 do
 
   def part1(input) do
     input
-    |> Enum.reduce(0, fn elem, acc ->
-      elem
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.sum()
-      |> max(acc)
-    end)
+    |> Enum.reduce(0, &(&1 |> transform_to_sum() |> max(&2)))
   end
 
   def part2(input) do
     input
-    # alternative lazy method with sorting and taking the first 3
-    # |> Enum.map(fn curr_str ->
-    #         curr_str
-    #         |> String.trim()
-    #         |> String.split("\n")
-    #         |> Enum.map(&String.to_integer/1)
-    #         |> Enum.sum()
-    #       end)
-    #    |> Enum.sort(&(&1 >= &2))
-    #    |> Enum.take(3)
-    #    |> Enum.sum
     |> Enum.reduce({0, 0, 0}, fn elem, acc ->
-      cals =
-        elem
-        |> String.split("\n", trim: true)
-        |> Enum.map(&String.to_integer/1)
-        |> Enum.sum()
+      cals = elem |> transform_to_sum()
 
       cond do
         cals > elem(acc, 0) -> {cals, elem(acc, 0), elem(acc, 1)}
@@ -55,5 +34,19 @@ defmodule Day1 do
       end
     end)
     |> Tuple.sum()
+
+    # alternative lazy method with sorting and taking the first 3
+    # input
+    # |> Enum.map(&transform_to_sum/1)
+    # |> Enum.sort(&(&1 >= &2))
+    # |> Enum.take(3)
+    # |> Enum.sum()
+  end
+
+  defp transform_to_sum(string) do
+    string
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 end
